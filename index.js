90deb36a07249169378a966d6b4f6cb997e947dd
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-02-04 16:56:13 +0500

    доделать JS

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let name = document.querySelector(".profile__name")
 let description = document.querySelector(".profile__description")
 let newName = document.querySelector(".edit-form__item_el_name");
 let newDescription = document.querySelector(".edit-form__item_el_description");
+let likeButtons = document.querySelectorAll(".card__button_like")
 
 
 
@@ -20,7 +21,6 @@ function openPopup() {
 function closePopup() {
   popup.classList.remove('popup_open');
   popup.classList.add('popup_hidden')
-
 }
 
 function submitPopup(evt) {
@@ -35,3 +35,14 @@ function submitPopup(evt) {
 editButton.addEventListener("click", openPopup)
 closeButton.addEventListener("click", closePopup)
 submitButton.addEventListener("click", submitPopup)
+for (let i = 0; i < likeButtons.length; i++) {
+  let likeButton = likeButtons[i];
+  likeButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    if (!likeButton.classList.contains('card__button_like_active')) {
+      likeButton.classList.add('card__button_like_active');
+    } else if (likeButton.classList.contains('card__button_like_active')) {
+      likeButton.classList.remove('card__button_like_active');
+    }
+  });
+}
