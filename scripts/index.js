7ca5f473999205b31fbf241e06fc159bb7f2d692
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-02-11 16:08:02 +0500

    Удалить лишнии классы popup

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@ const editForm = document.querySelector(".edit-form")
 const editButton = document.querySelector(".profile__button_type_edit")
 const popup = document.querySelector(".popup");
 const likeButtons = document.querySelectorAll(".card__button_like")
-const popupContainer = document.querySelector(".popup__container")
 let userName = document.querySelector(".profile__name")
 let description = document.querySelector(".profile__description")
 let newName = document.querySelector(".edit-form__item_el_name");
@@ -12,33 +11,26 @@ let newDescription = document.querySelector(".edit-form__item_el_description");
 
 function openPopup(evt) {
   evt.preventDefault();
-  popup.classList.remove('popup_hidden');
   popup.classList.add('popup_open');
   newName.value = userName.textContent;
   newDescription.value = description.textContent;
-  popupContainer.classList.add('popup__container_open')
-  popupContainer.classList.remove('popup__container_hidden')
 }
 
 function closePopup(evt) {
   evt.preventDefault();
   popup.classList.remove('popup_open');
-  popup.classList.add('popup_hidden')
-  popupContainer.classList.remove('popup__container_open')
-  
 }
 
-function submitPopup(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   userName.textContent = newName.value;
   description.textContent = newDescription.value;
   closePopup(evt);
 }
 
-
 editButton.addEventListener('click', openPopup)
 closeButton.addEventListener('click', closePopup)
-editForm.addEventListener('submit', submitPopup)
+editForm.addEventListener('submit', handleProfileFormSubmit)
 
 
 for (let i = 0; i < likeButtons.length; i++) {
